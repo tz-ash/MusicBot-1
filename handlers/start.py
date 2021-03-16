3b6a0b82b14c9111a04b468063c2caf8b5f95c17
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-03-16 11:26:36 +0530

    Add files via upload

---
+++ handlers/start.py
@@ -0,0 +1,70 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+import os
+import sys
+from threading import Thread
+from pyrogram import idle, filters
+from pyrogram.handlers import MessageHandler
+from helpers.wrappers import errors, admins_only
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+       f"""🤗🤗Hi {message.from_user.first_name}!
+
+😁 I am KINGBOT Music Player. 
+
+🥳 I can play music in your Telegram Group's Voice Chat😉
+
+⚜️ Use these buttons below to know more. 👇""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Source Code 😅 ", url="https://t.me/KINGBOTOFFICIAL"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group 💬", url="https://t.me/KINGBOTOFFICIALCHAT"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Channel 📣", url="https://t.me/KINGBOTOFFICIALSUPPORT"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "❌ Close ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "**KINGBOT:** I'm Working!!!\nUse me in Inline to search for a YouTube Video/Music. \n**Happy Streaming**",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🎶 Search 🎶", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "❌ Close ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
