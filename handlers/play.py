7834103ecf2a61cf3fb8e4dbd55a4f6206aafccf
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-04-15 07:28:01 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -1,79 +1,68 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+from os import path
 
-import tgcalls
-from converter import convert
-from youtube import download
-import sira
-from config import DURATION_LIMIT
-from helpers.wrappers import errors
+from pyrogram import Client
+from pyrogram.types import Message, Voice
+
+from callsmusic import callsmusic, queues
+
+import converter
+from downloaders import youtube
+
+from config import BOT_NAME as bn, DURATION_LIMIT
+from helpers.filters import command, other_filters
+from helpers.decorators import errors
 from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-
-@Client.on_message(
-    filters.command("play")
-    & filters.private
-    & ~ filters.edited
-)
-async def play_(client: Client, message: Message):
-    await message.reply_text("**KINGBOT:** Sorry! I can only be used in groups. \nTry again in a group.")
-
-
-@Client.on_message(
-    filters.command("play")
-    & filters.group
-    & ~ filters.edited
-)
+@Client.on_message(command("play") & other_filters)
 @errors
-async def play(client: Client, message_: Message):
-    audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
+async def play(_, message: Message):
 
-    res = await message_.reply_text("**KINGBOT:** 🔄 Processing...")
+    lel = await message.reply("**KINGBOT**: Processing....")
+    sender_id = message.from_user.id
+    sender_name = message.from_user.first_name
+
+    keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="🔊 Channel",
+                        url="https://t.me/KINGBOTOFFICIAL")
+                   
+                ]
+            ]
+        )
+
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    url = get_url(message)
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"**KINGBOT:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+                f"**KINGBOT**:❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play!"
             )
 
-        file_name = audio.file_id + audio.file_name.split(".")[-1]
-        file_path = await convert(await message_.reply_to_message.download(file_name))
+        file_name = get_file_name(audio)
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name)) else file_name
+        )
+    elif url:
+        file_path = await converter.convert(youtube.download(url))
     else:
-        messages = [message_]
-        text = ""
-        offset = None
-        length = None
+        return await lel.edit_text("**KINGBOT**:❗ You did not give me anything to play!")
 
-        if message_.reply_to_message:
-            messages.append(message_.reply_to_message)
-
-        for message in messages:
-            if offset:
-                break
-
-            if message.entities:
-                for entity in message.entities:
-                    if entity.type == "url":
-                        text = message.text or message.caption
-                        offset, length = entity.offset, entity.length
-                        break
-
-        if offset == None:
-            await res.edit_text("**KINGBOT:**❕ You did not give me anything to play.")
-            return
-
-        url = text[offset:offset+length]
-
-        file_path = await convert(download(url))
-
-    try:
-        is_playing = tgcalls.pytgcalls.is_playing(message_.chat.id)
-    except:
-        is_playing = False
-
-    if is_playing:
-        position = await sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"**KINGBOT:** #️⃣ Queued at position {position}.")
+    if message.chat.id in callsmusic.pytgcalls.active_calls:
+        position = await queues.put(message.chat.id, file=file_path)
+        await lel.edit(f"#⃣ **Queued** at position {position}!")
     else:
-        await res.edit_text("**KINGBOT:** ▶️ Playing...")
-        tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
+        callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+        await message.reply_photo(
+        photo="https://telegra.ph/file/796626d1106726dbfd061.jpg",
+        reply_markup=keyboard,
+        caption="**KINGBOT**: Playing...! here the song requested by {}!".format(
+        message.from_user.mention()
+        ),
+    )
+        return await lel.delete()
