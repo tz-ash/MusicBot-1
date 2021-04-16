20b70eccccad2859549c9ed69ebcc1858eb23746
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-04-16 15:30:12 +0530

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,7 +20,7 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"**KINGBOT**:⚠️ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"**⭐KINGBOT⭐**:⚠️ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
 
     ydl.download([url])
