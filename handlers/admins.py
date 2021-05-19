a152f6e891efefe0c196eff13cd925bf12c4e709
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-05-19 16:16:39 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,12 +1,31 @@
 from asyncio.queues import QueueEmpty
-
+from cache.admins import set
 from pyrogram import Client
 from pyrogram.types import Message
 from callsmusic import callsmusic
-
+import traceback
+import os
+import sys
+from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram import filters, emoji
 from config import BOT_NAME as BN
+from config import SUDO_USERS
 from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
+from config import que, admins as a
+
+@Client.on_message(filters.command('adminreset'))
+async def update_admin(client, message):
+    global a
+    admins = await client.get_chat_members(message.chat.id, filter="administrators")
+    new_ads = []
+    for u in admins:
+        new_ads.append(u.user.id)
+    a[message.chat.id] = new_ads
+    await message.reply_text('Sucessfully updated admin list in **{}**'.format(message.chat.title))
+
+
 
 
 @Client.on_message(command("pause") & other_filters)
@@ -18,10 +37,10 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text("**⭐KINGBOT⭐**: Nothing is playing!")
+        await message.reply_text("**⭐KINGBOT⭐**:❗ Nothing is playing!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: ▶️ Paused!!")
+        await message.reply_text("**⭐KINGBOT⭐**: ▶️ Paused!")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -33,10 +52,10 @@ async def resume(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
     ):
-        await message.reply_text("**⭐KINGBOT⭐**: 😏 Nothing is paused!")
+        await message.reply_text("**⭐KINGBOT⭐**: 😜 Nothing is paused!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: ⏸ Resumed!")
+        await message.reply_text("**⭐KINGBOT⭐**: ⏸ Resumed!!")
 
 
 @Client.on_message(command("end") & other_filters)
@@ -44,7 +63,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("**⭐KINGBOT⭐**: 🙄 Nothing is streaming!")
+        await message.reply_text("**⭐KINGBOT⭐**: 😅 Nothing is streaming!")
     else:
         try:
             callsmusic.queues.clear(message.chat.id)
@@ -52,15 +71,16 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: ❌ Stopped streaming!")
+        await message.reply_text("**⭐KINGBOT⭐**: 🙇Stopped streaming!")
 
 
 @Client.on_message(command("skip") & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
+    global que
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("**⭐KINGBOT⭐**: ❗ Nothing is playing to skip!")
+        await message.reply_text("**⭐KINGBOT⭐**:❗ Nothing is playing boss to skip!! I think its last song! or not playing song!!")
     else:
         callsmusic.queues.task_done(message.chat.id)
 
@@ -71,5 +91,20 @@ async def skip(_, message: Message):
                 message.chat.id,
                 callsmusic.queues.get(message.chat.id)["file"]
             )
+                
 
-        await message.reply_text("**⭐KINGBOT⭐**: ♂️ Skipped the current song!")
+    qeue = que.get(message.chat.id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"- ♂️ Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+
+
+@Client.on_message(
+    filters.command("admincache")
+)
+@errors
+async def admincache(client, message: Message):
+    set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
+    #await message.reply_text("**⭐KINGBOT⭐**: Admin cache refreshed!")
