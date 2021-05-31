131755e37d5e41201be171020558ca58ed71d577
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-05-31 20:33:05 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hlo dear 🥰, This is a music assistant service .\n\n 🙃 Rules:\n   - No chatting allowed\n  - You will get non reply beacause it is a bot\n  - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n This Is very easy and comfortablein ise with 0% risk!!\n    - If you have any issue and doubt 🧐 just contact @KINGBOTOFFICIALCHAT.\n   - Thanks For Using ⭐ KINGBOT ⭐ ☺️☺️\n\n")
+  await USER.send_message(message.chat.id,"Hlo dear [🥰](https://telegra.ph/file/f2c43863d300054125f22.jpg), This is a music assistant service .\n\n 🙃 Rules:\n   - No chatting allowed\n  - You will get non reply beacause it is a bot\n  - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n This Is very easy and comfortablein ise with 0% risk!!\n    - If you have any issue and doubt 🧐 just contact @KINGBOTOFFICIALCHAT.\n   - Thanks For Using ⭐ KINGBOT ⭐ ☺️☺️\n\n")
   return
