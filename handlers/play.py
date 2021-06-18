c4ded4104d85a8227b361f7910885213f38fe516
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-06-18 11:29:03 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -95,7 +95,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.close()
 
     image1 = Image.open("./background.png")
-    image2 = Image.open("KINGBOT/nobgplain.png")
+    image2 = Image.open("KINGBOT/thumbnail.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
